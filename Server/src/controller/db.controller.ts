abcedf3620ab4516ee95cafa8438ce9b6ad0816b
shubham pandey <shubhampaney3007@gmail.com>
2024-08-07 22:50:04 +0530

    Created first end to end prototype api

--- Server/src/controller/db.controller.ts
+++ Server/src/controller/db.controller.ts
@@ -1,15 +1,41 @@
 import { Request, Response } from "express";
 import { connection } from "../config/mysql.config";
 import run from "../geminiPrompt/prompt";
+import { FieldPacket, Pool, QueryResult } from "mysql2/promise";
+import getAnswer from "../geminiPrompt/answerPrompt";
+
+
+async function getMdata(tableName : string , pool : Pool) : Promise<string>{
+  try{
+    const [rows] : [QueryResult, FieldPacket[]] = await pool.query('SELECT MDATA FROM STORE_META WHERE TABLENAME = ?', [tableName]);
+    const jsonObj = JSON.parse(JSON.stringify(rows));
+    return jsonObj[0].MDATA;
+  }
+  catch(error: any)
+  {
+    console.error(`Error executing query : ${error.stack}`)
+  }
+
+  return ""
+}
 
 export async function postQuery(req: Request, res: Response): Promise<void> {
+  const tableName: string =  req.body.tableName;
   const question: string = req.body.question;
-  console.log(question);
+  console.log(tableName);
 
   const pool = await connection();
-  const query: string = await run(question, "testTable1");
+
+  // Getting the metaData for the table
+  const metadata: string = await getMdata(tableName , pool);
+
+  const query: string = await run(question, tableName , metadata);
   console.log(query);
-  const result: any = await pool.query(query);
-  res.json(result[0]);
-  console.log(result[0]);
+  const queryResult: any = await pool.query(query);
+  console.log(`the queryResult is : ${JSON.stringify(queryResult[0])}`);
+
+  // Getting back the answer
+  const answer = await getAnswer(question , metadata , JSON.stringify(queryResult[0]))
+  res.json(answer);
+  console.log(answer);
 }
--- Server/src/geminiPrompt/prompt.ts
+++ Server/src/geminiPrompt/prompt.ts
@@ -4,34 +4,37 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY || "");
 
 export default async function run(
   question: string,
-  table_name: string
+  table_name: string,
+  metadata?: string
 ): Promise<string> {
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-  const prompt = `
+  const queryPrompt = `
     
-    - System : You are an SQL Query Writer application and you create SQL Queries according to the Question asked by the user. You know about the table from the given Schema of the table. 
+    - System : You are an SQL Query Writer application and you create SQL Queries according to the Question asked by the user. If the tablename has special characters in it , you must enclose it within backticks while writing the Query.
+        You know about the table from the given Schema of the table. 
         You are also given a random Example for reference.
 
-    - Schema : (SNo Int (Primary Key) , Name varchar(50) , Department varchar(50)); || Tablename : ${table_name}
+    - Schema : ${metadata} || Tablename : ${table_name}
 
     - Example
         User : get me all the records from ${table_name}
 
-        Answer : select * from ${table_name};
+        Answer : select * from \`${table_name}\`;
 
-    - Note : If no Sql Query can be created from 'Question' , then return the Query : "select * from ${table_name};"
+    - Note : If no Sql Query can be created from 'Question' , then return the Query : "select * from \`${table_name}\`;"
 
 
     - Question
         User : ${question}
 
-    - Answer :
+    - Answer :text
     
     `;
 
-  const result = await model.generateContent(prompt);
+  const result = await model.generateContent(queryPrompt);
   const response = await result.response;
   const text = response.text();
-  return text;
+
+  return text
 }
