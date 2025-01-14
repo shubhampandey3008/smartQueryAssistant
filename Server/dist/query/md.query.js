15ed8e9c7316d8c4bf11662a8c767c62a5bd093a
shubhampandey3008 <shubhampaney3007@gmail.com>
2025-01-14 11:14:08 +0530

    Changed the vercel.json configurations

--- Server/dist/query/md.query.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.QUERY = void 0;
-exports.QUERY = {
-    CREATE_TABLE: "CREATE TABLE ? ? ;",
-    INSERT_VALUE: "INSERT INTO ? VALUES(?) ;",
-    DROP_TABLE: "DROP TABLE ? ;",
-};
--- Server/dist/routes/db.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const db_controller_1 = require("../controller/db.controller");
-const dbRouter = (0, express_1.Router)();
-dbRouter.post("/", db_controller_1.postQuery);
-dbRouter.post("/plot", db_controller_1.postPlotQuery);
-dbRouter.post("/show", db_controller_1.showTable);
-exports.default = dbRouter;
